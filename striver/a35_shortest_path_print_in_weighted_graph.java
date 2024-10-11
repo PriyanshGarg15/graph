a3fc09a817cc75c60b3e48e710524e2f5c53fe5f
priyanshgarg15 <priyanshgargagra123@gmail.com>
2024-10-11 13:29:16 +0530

    first commit

--- striver/a35_shortest_path_print_in_weighted_graph.java
+++ striver/a35_shortest_path_print_in_weighted_graph.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class a35_shortest_path_print_in_weighted_graph 
 {
-     class Pair implements Comparable<Pair> {
-        int distance;  // Distance
-        int vertex; // Node
+    public class Pair implements Comparable<Pair> {
+        int distance;
+        int vertex;
     
         public Pair(int distance, int vertex) {
             this.distance = distance;
@@ -22,8 +22,7 @@ public class a35_shortest_path_print_in_weighted_graph
         }
     }
 
-    public static List<Integer> shortestPath(int n, int m, int edges[][]) {
-        // Create an adjacency list of pairs of the form node1 -> {node2, edge weight}
+    public List<Integer> shortestPath(int n, int m, int edges[][]) {  //m (the number of edges) is equivalent to edges.length //n is number of vertices
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>(); 
         for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>()); 
@@ -33,10 +32,8 @@ public class a35_shortest_path_print_in_weighted_graph
             adj.get(edges[i][1]).add(new Pair(edges[i][0], edges[i][2])); 
         }
         
-        // Create a priority queue for storing the nodes along with distances 
         PriorityQueue<Pair> pq = new PriorityQueue<>();
 
-        // Create a dist array for storing the updated distances and a parent array
         int[] dist = new int[n + 1]; 
         int[] parent = new int[n + 1]; 
         Arrays.fill(dist, (int) (1e9)); 
@@ -44,9 +41,9 @@ public class a35_shortest_path_print_in_weighted_graph
             parent[i] = i; 
         }
         
-        dist[1] = 0; // Source distance initialized to 0
+        dist[1] = 0;
 
-        // Push the source node to the queue
+     
         pq.add(new Pair(0, 1));
 
         while (!pq.isEmpty()) {
@@ -63,23 +60,25 @@ public class a35_shortest_path_print_in_weighted_graph
                 }
             }
         }
+
         List<Integer> path = new ArrayList<>();  
 
         // If distance to a node could not be found, return an array containing -1
+        // you are checking the distance to vertex n (the destination vertex) to determine if it is reachable from the source vertex.
         if (dist[n] == 1e9) {
             path.add(-1); 
             return path; 
         }
         
-        // Backtrack to find the path
+
         int node = n;
         while (parent[node] != node) {
             path.add(node); 
             node = parent[node]; 
         }
+        
         path.add(1); // Add the source node to the path
 
-        // Reverse the path to get the final answer and return it
         Collections.reverse(path); 
         return path;
     }
